dc9014772e9fe03e7b6e0e04a715f10f9df16068
shande007 <shande.personal@gmail.com>
2026-01-07 18:07:43 +0530

    Initial commit

---
+++ src/Conditional_Statements/Prog13_If_Else_Conditional.java
@@ -0,0 +1,23 @@
+package Conditional_Statements;
+
+public class Prog13_If_Else_Conditional {
+    public static void main(String[] args) {
+
+        int a=10, b=40,c=30;
+
+        if(a>b && a>c)
+        {
+            System.out.println("a is largest number");
+        }
+        else if(b>a && b>c)
+        {
+            System.out.println("b is largest number");
+        }
+        else
+        {
+            System.out.println("c is largest number");
+        }
+
+    }
+
+}
