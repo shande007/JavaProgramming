119269901d7d2274ee7368e5e08d962491ff1b99
shande007 <shande.personal@gmail.com>
2026-01-07 17:34:01 +0530

    If else conditional statements

---
+++ src/Conditional_Statements/Prog11_If_Else_Condition.java
@@ -0,0 +1,18 @@
+package Conditional_Statements;
+
+public class Prog11_If_Else_Condition {
+    public static void main(String[] args) {
+
+        int person_age=10;
+
+        if(person_age>=18)
+        {
+            System.out.println("Eligible for vote");
+        }
+        else
+        {
+            System.out.println("Not eligible for vote");
+        }
+
+    }
+}
