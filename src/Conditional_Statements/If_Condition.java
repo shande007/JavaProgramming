827e5e104a5651392efc6daebeb4a1a5000f1c2f
shande007 <shande.personal@gmail.com>
2026-01-07 17:30:27 +0530

    If condition

---
+++ src/Conditional_Statements/If_Condition.java
@@ -0,0 +1,14 @@
+package Conditional_Statements;
+
+public class If_Condition {
+    public static void main(String[] args) {
+
+        int person_age=20;
+
+        if(person_age>=18)
+        {
+            System.out.println("Eligible for vote");
+        }
+
+    }
+}
