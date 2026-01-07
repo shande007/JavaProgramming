5506863a7ec8395afa7537794a0490166da9f38d
shande007 <shande.personal@gmail.com>
2026-01-07 19:05:10 +0530

    Initial commit

---
+++ src/Conditional_Statements/Prog18_If_Else_Try.java
@@ -0,0 +1,22 @@
+package Conditional_Statements;
+
+public class Prog18_If_Else_Try {
+    public static void main(String[] args) {
+
+        int a = -45, b = -45;
+
+        if(a>b)
+        {
+            System.out.println("a is greater than b");
+        }
+        else
+        {
+            if (a == b) {
+                System.out.println("Both a and b is equal");
+            }
+            else {
+                System.out.println("b is greater than a");
+            }
+        }
+    }
+}
---
+++ src/Conditional_Statements/Prog19_SmallestNumFromThree.java
@@ -0,0 +1,23 @@
+package Conditional_Statements;
+
+public class Prog19_SmallestNumFromThree {
+    public static void main(String[] args) {
+        int a = 10, b = 10, c = 2;
+
+        if (a == b && b == c) {
+            System.out.println("All three numbers are equal: " + a);
+        } else if (a == b && a < c) {
+            System.out.println("a and b are equal and smallest: " + a);
+        } else if (a == c && a < b) {
+            System.out.println("a and c are equal and smallest: " + a);
+        } else if (b == c && b < a) {
+            System.out.println("b and c are equal and smallest: " + b);
+        } else if (a < b && a < c) {
+            System.out.println("Smallest number is: " + a);
+        } else if (b < a && b < c) {
+            System.out.println("Smallest number is: " + b);
+        } else {
+            System.out.println("Smallest number is: " + c);
+        }
+    }
+}
