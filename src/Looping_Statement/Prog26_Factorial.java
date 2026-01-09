ce96039b82e8d28418f841285141004c6a4f8146
shande007 <shande.personal@gmail.com>
2026-01-09 19:50:44 +0530

    Initial Commit

---
+++ src/Looping_Statement/Prog26_Factorial.java
@@ -0,0 +1,13 @@
+package Looping_Statement;
+
+public class Prog26_Factorial {
+    public static void main(String[] args) {
+        int num = 5, fact = 1;
+
+        for (int i = 1; i <= num; i++) {
+            fact *= i;
+        }
+
+        System.out.println("Factorial of " + num +" = " + fact);
+    }
+}
---
+++ src/Looping_Statement/Prog29_CountDigits.java
@@ -0,0 +1,14 @@
+package Looping_Statement;
+
+public class Prog29_CountDigits {
+    public static void main(String[] args) {
+        int num = 12345, count = 0;
+
+        while (num != 0) {
+            count++;
+            num /= 10;
+        }
+
+        System.out.println("Digits = " + count);
+    }
+}
