3c0f0b0b64419a3b191c46173882720e20c8879a
shande007 <shande.personal@gmail.com>
2026-01-12 15:09:44 +0530

    Comments modified

---
+++ src/Operators/Prog30_Inc_decre_ex.java
@@ -0,0 +1,11 @@
+package Operators;
+
+public class Prog30_Inc_decre_ex {
+    public static void main(String[] args) {
+
+        int a = 10;
+        a = a++ + a++ + a++;
+        System.out.println(a);
+
+    }
+}
---
+++ src/Operators/Prog31_Inc_Dec_ex2.java
@@ -0,0 +1,13 @@
+package Operators;
+
+public class Prog31_Inc_Dec_ex2 {
+    public static void main(String[] args) {
+
+        int a = 3;
+
+        int b = a++ * ++a;
+
+        System.out.println(b);
+
+    }
+}
