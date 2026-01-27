d4e9f5a7c0f3e1afbcf85d493540a32470078df4
shande007 <shande.personal@gmail.com>
2026-01-27 18:48:29 +0530

    New progms

---
+++ src/Array_Single/Prog55_MaxArray.java
@@ -0,0 +1,22 @@
+package Array_Single;
+
+public class Prog55_MaxArray {
+    public static void main(String[] args) {
+
+        int[] array = {25, 14, 56, 15, 36, 56, 77, 18, 29, 49};
+//        Arrays.sort(array); // 14,15,18,25,29,36,49,56,77
+//        System.out.println(array[array.length-1]);
+
+        int max = array[0];
+        int pos = 0;
+        for(int i = 0 ; i < array.length; i++)
+        {
+            if(array[i] > max){
+                max = array[i];
+                pos = i+1;
+            }
+        }
+        System.out.println("Minimum from given array is "+ max+" and found at position "+ pos);
+
+       }
+}
