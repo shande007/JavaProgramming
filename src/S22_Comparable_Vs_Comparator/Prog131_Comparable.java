216461838c0a6a7fe413a4a94c056412f88e0329
shande007 <shande.personal@gmail.com>
2026-03-09 20:03:56 +0530

    Colletion Frameworks

---
+++ src/S22_Comparable_Vs_Comparator/Prog131_Comparable.java
@@ -0,0 +1,75 @@
+package S22_Comparable_Vs_Comparator;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Prog131_Comparable {
+
+
+    public static void main(String[] args) {
+        Employee e1 = new Employee(3, "Rutuja");
+        Employee e2 = new Employee(1, "Sidhant");
+        Employee e3 = new Employee(5, "Hande");
+
+        List<Employee> employeeList = new ArrayList<>();
+        employeeList.add(e1);
+        employeeList.add(e2);
+        employeeList.add(e3);
+        System.out.println(employeeList);
+
+        // Sort them based on the id
+        Collections.sort(employeeList);
+//        System.out.println(employeeList);
+
+        // Sort them based on the name?
+        System.out.println(employeeList);
+
+    }
+
+}
+
+class Employee implements Comparable<Employee> {
+    private Integer id;
+    private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public String printDetails() {
+        return id + " - " + name;
+    }
+
+    public Employee(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    @Override
+    public int compareTo(Employee o) {
+//        return this.id-o.id;
+        return this.name.compareTo(o.name);
+    }
+}
